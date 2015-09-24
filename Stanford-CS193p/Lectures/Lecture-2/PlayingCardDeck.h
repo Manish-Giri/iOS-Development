92bbd86637fb729de850fa22bc0d13be9af8ef69
Manish Giri <manish.giri.me@gmail.com>
2015-09-23 21:15:02 -0400

    PlayingCardDeck header file
    PlayingCardDeck is the subclass of Deck that represents the current deck
    being played with.

---
+++ Stanford-CS193p/Lectures/Lecture-2/PlayingCardDeck.h
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.h
+//  CardGame
+//
+//  Created by Manish Giri on 9/21/15.
+//  Copyright (c) 2015 Manish Giri. All rights reserved.
+//
+
+#import "Deck.h"
+#import "PlayingCard.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
