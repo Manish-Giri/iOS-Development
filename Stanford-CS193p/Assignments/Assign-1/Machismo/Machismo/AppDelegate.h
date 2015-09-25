727aac8e644eeda908203b3a4b07ffa5a3b696b1
Manish-Giri <manish.giri.me@gmail.com>
2015-09-25 19:59:14 -0400

    AppDelegate header file

---
+++ Stanford-CS193p/Assignments/Assign-1/Machismo/Machismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Machismo
+//
+//  Created by Manish Giri on 9/22/15.
+//  Copyright (c) 2015 Manish Giri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
