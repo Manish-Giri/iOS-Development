6c6625716f8cdcd2c5cf3910afecc246c424a296
Manish-Giri <manish.giri.me@gmail.com>
2015-09-24 20:19:25 -0400

    PlayingCardDeck class header file to machismo sub directory

---
+++ Stanford-CS193p/Assignments/Assign-1/Machismo/Machismo/PlayingCardDeck.h
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.h
+//  Machismo
+//
+//  Created by Manish Giri on 9/22/15.
+//  Copyright (c) 2015 Manish Giri. All rights reserved.
+//
+
+#import "Deck.h"
+#import "PlayingCard.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
