ba61ef252af9bc32f80828bb56674ad510b1a729
Manish-Giri <manish.giri.me@gmail.com>
2015-09-25 20:00:42 -0400

    main.m
    to start things off

---
+++ Stanford-CS193p/Assignments/Assign-1/Machismo/Machismo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Machismo
+//
+//  Created by Manish Giri on 9/22/15.
+//  Copyright (c) 2015 Manish Giri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
